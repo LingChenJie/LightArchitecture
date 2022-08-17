049adab440c8be3ca02bd44756f388bd23823e6c
lingchenjie <976918915@qq.com>
2022-08-17 23:36:18 +0800

    优化mvi

--- architecture/src/main/java/com/android/architecture/domain/dispatcher/MviDispatcher.kt
+++ architecture/src/main/java/com/android/architecture/domain/dispatcher/MviDispatcher.kt
@@ -1,14 +1,14 @@
 package com.android.architecture.domain.dispatcher
 
+import android.util.SparseArray
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.util.isNotEmpty
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.*
 import com.android.architecture.helper.Logger
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 import java.util.concurrent.atomic.AtomicInteger
@@ -18,40 +18,33 @@ import java.util.concurrent.atomic.AtomicInteger
  */
 abstract class MviDispatcher<E> : ViewModel() {
     companion object {
-        private const val DEFAULT_QUEUE_LENGTH = 10
+        private const val DEFAULT_QUEUE_LENGTH = 1
         private const val START_VERSION = -1
     }
 
     protected val TAG = this.javaClass.simpleName
     private val _sharedFlow: MutableSharedFlow<E> by lazy {
         MutableSharedFlow(
-            replay = 1,
+            replay = initQueueMaxLength(),
             onBufferOverflow = BufferOverflow.DROP_OLDEST,
             extraBufferCapacity = initQueueMaxLength()
         )
     }
-    private val delayMap: MutableMap<Int, Boolean> = mutableMapOf()
-    private val lastValue = LastValue(0)
+    private val delayArray: SparseArray<Boolean> = SparseArray()
     private val currentVersion = AtomicInteger(START_VERSION)
-
 
     protected open fun initQueueMaxLength(): Int {
         return DEFAULT_QUEUE_LENGTH
     }
 
     fun output(activity: AppCompatActivity, observer: (E) -> Unit) {
-        delayMap[System.identityHashCode(activity)] = true
+        delayArray[System.identityHashCode(activity)] = true
         val observerWrapper = ObserverWrapper(observer, currentVersion.get())
         activity.lifecycleScope.launch {
             activity.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                delayMap.remove(System.identityHashCode(activity))
-//                _sharedFlow.flowConsumeOnce().collect {
-//                    Logger.i(TAG, "----collect: $activity")
-//                    observer.invoke(it)
-//                }
+                delayArray.remove(System.identityHashCode(activity))
                 _sharedFlow.collect {
                     Logger.i(TAG, "----collect: $activity")
-                    //observer.invoke(it)
                     observerWrapper.onChanged(it)
                 }
             }
@@ -59,18 +52,13 @@ abstract class MviDispatcher<E> : ViewModel() {
     }
 
     fun output(fragment: Fragment, observer: (E) -> Unit) {
-        delayMap[System.identityHashCode(fragment)] = true
+        delayArray[System.identityHashCode(fragment)] = true
         val observerWrapper = ObserverWrapper(observer, currentVersion.get())
         fragment.viewLifecycleOwner.lifecycleScope.launch {
             fragment.viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                delayMap.remove(System.identityHashCode(fragment))
-//                _sharedFlow.flowConsumeOnce().collect {
-//                    Logger.i(TAG, "----collect: $fragment")
-//                    observer.invoke(it)
-//                }
+                delayArray.remove(System.identityHashCode(fragment))
                 _sharedFlow.collect {
                     Logger.i(TAG, "----collect: $fragment")
-                    //observer.invoke(it)
                     observerWrapper.onChanged(it)
                 }
             }
@@ -78,11 +66,11 @@ abstract class MviDispatcher<E> : ViewModel() {
     }
 
     protected suspend fun sendResult(event: E) {
+        currentVersion.getAndIncrement()
         _sharedFlow.emit(event)
     }
 
     fun input(event: E) {
-        currentVersion.getAndIncrement()
         viewModelScope.launch {
             if (needDelayForLifecycleState) delayForLifecycleState().collect { onHandle(event) }
             else onHandle(event)
@@ -92,7 +80,7 @@ abstract class MviDispatcher<E> : ViewModel() {
     protected abstract suspend fun onHandle(event: E)
 
     private val needDelayForLifecycleState
-        get() = delayMap.isNotEmpty()
+        get() = delayArray.isNotEmpty()
 
     private fun delayForLifecycleState() = flow {
         delay(1)
@@ -106,17 +94,5 @@ abstract class MviDispatcher<E> : ViewModel() {
             }
         }
     }
-
-    private fun <E> Flow<E>.flowConsumeOnce(): Flow<E> = callbackFlow {
-        this@flowConsumeOnce.collect {
-            val newHashCode = System.identityHashCode(it)
-            if (lastValue.hashCode != newHashCode) send(it)
-            lastValue.hashCode = newHashCode
-        }
-        lastValue.hashCode = 0
-        close()
-    }
-
-    data class LastValue(var hashCode: Int)
 
 }
