7c5471d4fb21ab4b3f5493bfb3fef4a78cf75b3d
suqi <976918915@qq.com>
2022-08-31 10:23:55 +0800

    更新HttpRequest

--- architecture/src/main/java/com/android/architecture/data/remote/HttpRequest.java
+++ architecture/src/main/java/com/android/architecture/data/remote/HttpRequest.java
@@ -26,59 +26,105 @@ import okhttp3.logging.HttpLoggingInterceptor;
 public class HttpRequest {
 
     private static final String TAG = HttpRequest.class.getSimpleName();
-    private static final int CONNECT_TIME_OUT = 5;// 连接超时时间
-    private static final int COMMUNICATION_TIME_OUT = 38;// 超时时间设置为38秒
+    private int timeout = 38;// 超时时间
+    private String url;// 通讯地址
+    private final OkHttpClient okHttpClient;
 
-
-    public static OkHttpClient getOkHttpClient() {
-        return getOkHttpClient(COMMUNICATION_TIME_OUT);
+    public void setTimeout(int timeout) {
+        this.timeout = timeout;
     }
 
-    public static OkHttpClient getOkHttpClient(int timeout) {
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public HttpRequest() {
         HttpsUtils.SSLParams sslParams = HttpsUtils.getSslSocketFactory();
-        OkHttpClient client = new OkHttpClient()
+        okHttpClient = new OkHttpClient()
                 .newBuilder()
-                .connectTimeout(CONNECT_TIME_OUT, TimeUnit.SECONDS)
+                .connectTimeout(timeout, TimeUnit.SECONDS)
                 .writeTimeout(timeout, TimeUnit.SECONDS)
                 .readTimeout(timeout, TimeUnit.SECONDS)
                 .addNetworkInterceptor(getLogInterceptor())
                 .sslSocketFactory(sslParams.sslSocketFactory, sslParams.trustManager)
                 .hostnameVerifier(new TrustAllHostname())
                 .build();
-        return client;
+    }
+
+    public HttpRequest(OkHttpClient okHttpClient) {
+        this.okHttpClient = okHttpClient;
+    }
+
+    /**
+     * GET 请求
+     *
+     * @return 结果
+     * @throws Exception
+     */
+    public String get() throws Exception {
+        return get(null);
+    }
+
+    /**
+     * GET 请求
+     *
+     * @param headers 报文头
+     * @return 结果
+     * @throws Exception
+     */
+    public String get(Map<String, String> headers) throws Exception {
+        if (url == null) {
+            throw new RuntimeException("url cannot be null");
+        }
+        Logger.i(TAG, "Get start");
+        Request.Builder requestBuilder = new Request.Builder().url(url).get();
+        addHeader(requestBuilder, headers);
+        Request request = requestBuilder.build();
+        Logger.d(TAG, "Get Req --> url:" + url);
+        Response response = okHttpClient.newCall(request).execute();
+        Logger.i(TAG, "Get end");
+
+        if (response.isSuccessful()) {
+            String result = response.body().string();
+            Logger.d(TAG, "Get Res --> " + result);
+            return result;
+        } else {
+            throw new IOException("Get code " + response);
+        }
     }
 
     /**
      * POST 请求，json方式
      *
-     * @param url     请求地址
-     * @param content 请求数据
+     * @param jsonStr 请求数据
      * @return 结果
      * @throws Exception
      */
-    public static String postJson(String url, String content) throws Exception {
-        return postJson(url, null, content, COMMUNICATION_TIME_OUT);
+    public String postJson(String jsonStr) throws Exception {
+        return postJson(null, jsonStr);
     }
 
     /**
      * POST 请求，json方式
      *
-     * @param url     请求地址
-     * @param content 请求数据
-     * @param timeout 超时时间
+     * @param headers 报文头
+     * @param jsonStr 请求数据
      * @return 结果
      * @throws Exception
      */
-    public static String postJson(String url, Map<String, String> headers, String content, int timeout) throws Exception {
+    public String postJson(Map<String, String> headers, String jsonStr) throws Exception {
+        if (url == null) {
+            throw new RuntimeException("url cannot be null");
+        }
         Logger.i(TAG, "PostJson start");
         //RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), content);
         MediaType mediaType = MediaType.Companion.parse("application/json; charset=utf-8");
-        RequestBody body = RequestBody.Companion.create(content, mediaType);
+        RequestBody body = RequestBody.Companion.create(jsonStr, mediaType);
         Request.Builder requestBuilder = new Request.Builder().url(url).post(body);
         addHeader(requestBuilder, headers);
         Request request = requestBuilder.build();
-        Logger.d(TAG, "Http Req --> " + url + content);
-        Response response = getOkHttpClient(timeout).newCall(request).execute();
+        Logger.d(TAG, "Http Req --> url:" + url + "; jsonStr:" + jsonStr);
+        Response response = okHttpClient.newCall(request).execute();
         Logger.i(TAG, "PostJson end");
 
         if (response.isSuccessful()) {
@@ -93,28 +139,28 @@ public class HttpRequest {
     /**
      * POST 请求，表单方式
      *
-     * @param url     请求地址
-     * @param content 请求数据
+     * @param params 请求数据
      * @return 结果
      * @throws Exception
      */
-    public static String postForm(String url, Map<String, String> content) throws Exception {
-        return postForm(url, null, content, COMMUNICATION_TIME_OUT);
+    public String postForm(Map<String, String> params) throws Exception {
+        return postForm(null, params);
     }
 
     /**
      * POST 请求，表单方式
      *
-     * @param url     请求地址
-     * @param content 请求数据
-     * @param timeout 超时时间
+     * @param params 请求数据
      * @return 结果
      * @throws Exception
      */
-    public static String postForm(String url, Map<String, String> headers, Map<String, String> content, int timeout) throws Exception {
+    public String postForm(Map<String, String> headers, Map<String, String> params) throws Exception {
+        if (url == null) {
+            throw new RuntimeException("url cannot be null");
+        }
         Logger.i(TAG, "PostForm start");
         FormBody.Builder bodyBuilder = new FormBody.Builder();
-        Set<Map.Entry<String, String>> entries = content.entrySet();
+        Set<Map.Entry<String, String>> entries = params.entrySet();
         for (Map.Entry<String, String> entry : entries) {
             if (entry.getValue() != null) {
                 bodyBuilder.add(entry.getKey(), entry.getValue());
@@ -124,8 +170,8 @@ public class HttpRequest {
         Request.Builder requestBuilder = new Request.Builder().url(url).post(body);
         addHeader(requestBuilder, headers);
         Request request = requestBuilder.build();
-        Logger.d(TAG, "Http Req --> " + url + content);
-        Response response = getOkHttpClient(timeout).newCall(request).execute();
+        Logger.d(TAG, "Http Req --> url:" + url);
+        Response response = okHttpClient.newCall(request).execute();
         Logger.i(TAG, "PostForm end");
 
         if (response.isSuccessful()) {
