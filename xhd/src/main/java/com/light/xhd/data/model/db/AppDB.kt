a4d7ca3b68df297f143e025ce1ad3e8f91d722fd
lingchenjie <976918915@qq.com>
2022-09-01 08:12:47 +0800

    commit code

--- xhd/src/main/java/com/light/xhd/data/model/db/AppDB.kt
+++ xhd/src/main/java/com/light/xhd/data/model/db/AppDB.kt
@@ -3,7 +3,9 @@ package com.light.xhd.data.model.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.light.xhd.data.model.db.dao.NoteDao
+import com.light.xhd.data.model.db.dao.UserInfoDao
 import com.light.xhd.data.model.db.entity.Note
+import com.light.xhd.data.model.db.entity.UserInfo
 
 /**
  * File describe:
@@ -12,7 +14,8 @@ import com.light.xhd.data.model.db.entity.Note
  * Modify date: 2022/7/30
  * Version: 1
  */
-@Database(entities = [Note::class], version = 1, exportSchema = true)
+@Database(entities = [Note::class, UserInfo::class], version = 1, exportSchema = true)
 abstract class AppDB : RoomDatabase() {
     abstract fun noteDao(): NoteDao
+    abstract fun userInfoDao(): UserInfoDao
 }
---
+++ xhd/src/main/java/com/light/xhd/data/model/db/entity/TransData.kt
@@ -0,0 +1,18 @@
+package com.light.xhd.data.model.db.entity
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+/**
+ * Created by SuQi on 2022/9/1.
+ * Describe:
+ */
+@Entity
+@Parcelize
+data class TransData(
+    @PrimaryKey(autoGenerate = true)
+    val tId: Long,
+) : Parcelable {
+}
