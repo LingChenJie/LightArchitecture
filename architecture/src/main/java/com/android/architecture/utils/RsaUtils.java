908a22d61fc860a7f8b6fccf6626ed149661296a
suqi <976918915@qq.com>
2022-12-17 18:29:51 +0800

    feat:code commit

--- architecture/src/main/java/com/android/architecture/utils/RsaUtils.java
+++ architecture/src/main/java/com/android/architecture/utils/RsaUtils.java
@@ -173,27 +173,27 @@ public class RsaUtils {
     }
 
     public static void main(String[] args) {
-//        KeyPair keyPair = generateRSAKeyPair(DEFAULT_KEY_SIZE);
-//        byte[] privateKey = getPrivateKey(keyPair);
-//        byte[] publicKey = getPublicKey(keyPair);
-//        System.out.println("privateKey:" + ByteUtil.bytes2HexString(privateKey));
-//        System.out.println("publicKey:" + ByteUtil.bytes2HexString(publicKey));
+        KeyPair keyPair = generateRSAKeyPair(DEFAULT_KEY_SIZE);
+        byte[] privateKey = getPrivateKey(keyPair);
+        byte[] publicKey = getPublicKey(keyPair);
+        System.out.println("privateKey:" + ByteUtil.bytes2HexString(privateKey));
+        System.out.println("publicKey:" + ByteUtil.bytes2HexString(publicKey));
+
+        byte[] data = ByteUtil.hexString2Bytes("11223344556677881122334455667788");
+        byte[] encryptResult = encryptByPublicKey(publicKey, data);
+        System.out.println("encryptResult:" + ByteUtil.bytes2HexString(encryptResult));
+
+        byte[] decryptResult = decryptByPrivateKey(privateKey, encryptResult);
+        System.out.println("decryptResult:" + ByteUtil.bytes2HexString(decryptResult));
+
+
+//        String module = "c0301ed3994f78d29b5282230ca43b493d4a55f0daf5944369d4d5d2add167bc86b812b4c11a1b17a1ceaee39530ca05e67ef12bce88859d217311fff2db06684fe7aa400fbf4dfdb6ede6780ac277c01751a41d4c558ee9259236520e84d9a2f89530cd74c00f974c2ba3a1f85cb225e1792d81cee7af6f76d6b3edb5d9cc7fcc0d39000a7d997077b054ed1d6f753191804c3e8e0b26a64c7b9449f275e89248d917e788ee8f63bae94c235f85ac976c90e4a8894934bb721b04c2dff53dae9a2b42b642f9db2015ef2fc67412a7a48333c19d4f62ca6ed90de35db1249cac3a197d762c7ba4a0384a950e5f73ae472c7bd72ac4bf51276e4f5bef3b278b07";
+//        String exponent = "300f9895e7dff4e20e0f9b15b2c29b44dc7beb73f8fdb2df0ce739b59edc202a08329e12efbb5c8d135cc1658e588e1cdda05f254a57dfba04b4670aefab9035a3cdd64880e7a154525bb9e7c9210a9b51f98ba5fc01e5642e8e1ae1785a06a6f7ad2f7021c329e2f71c6ea6419ac4be2e5d8460d2e223802762d7ca1950e70f9acf9a9ae12206180553f8837e9cffa212bde1e209f49ed793cc2028bee76e5ce035f10dcf9f6ec3fea98fd2213883780a88847c7892a69b5382425bc1aef627c35b83d0a39902c9b89816b7c28cfbdf41625e57e74fbeb417425020d18eddca0ccb1f3740529e59adee1d38d3d93293665c9e7485f24c47bba7c2cd2ab52591";
+//        byte[] privateKey = getPrivateKey(module, exponent);
 //
-//        byte[] data = ByteUtil.hexString2Bytes("11223344556677881122334455667788");
-//        byte[] encryptResult = encryptByPublicKey(publicKey, data);
-//        System.out.println("encryptResult:" + ByteUtil.bytes2HexString(encryptResult));
-//
-//        byte[] decryptResult = decryptByPrivateKey(privateKey, encryptResult);
-//        System.out.println("decryptResult:" + ByteUtil.bytes2HexString(decryptResult));
-
-
-        String module = "c0301ed3994f78d29b5282230ca43b493d4a55f0daf5944369d4d5d2add167bc86b812b4c11a1b17a1ceaee39530ca05e67ef12bce88859d217311fff2db06684fe7aa400fbf4dfdb6ede6780ac277c01751a41d4c558ee9259236520e84d9a2f89530cd74c00f974c2ba3a1f85cb225e1792d81cee7af6f76d6b3edb5d9cc7fcc0d39000a7d997077b054ed1d6f753191804c3e8e0b26a64c7b9449f275e89248d917e788ee8f63bae94c235f85ac976c90e4a8894934bb721b04c2dff53dae9a2b42b642f9db2015ef2fc67412a7a48333c19d4f62ca6ed90de35db1249cac3a197d762c7ba4a0384a950e5f73ae472c7bd72ac4bf51276e4f5bef3b278b07";
-        String exponent = "300f9895e7dff4e20e0f9b15b2c29b44dc7beb73f8fdb2df0ce739b59edc202a08329e12efbb5c8d135cc1658e588e1cdda05f254a57dfba04b4670aefab9035a3cdd64880e7a154525bb9e7c9210a9b51f98ba5fc01e5642e8e1ae1785a06a6f7ad2f7021c329e2f71c6ea6419ac4be2e5d8460d2e223802762d7ca1950e70f9acf9a9ae12206180553f8837e9cffa212bde1e209f49ed793cc2028bee76e5ce035f10dcf9f6ec3fea98fd2213883780a88847c7892a69b5382425bc1aef627c35b83d0a39902c9b89816b7c28cfbdf41625e57e74fbeb417425020d18eddca0ccb1f3740529e59adee1d38d3d93293665c9e7485f24c47bba7c2cd2ab52591";
-        byte[] privateKey = getPrivateKey(module, exponent);
-
-        byte[] en = ByteUtil.hexString2Bytes("124DCD2654F46B4296BE416D1B63568AF74A196DD2A2FB993AA6E3D024C6C95AD74F423FA9D48381C09A9807A285F4A37E8C9ABC3974FEF29294FD66563B3DAB8067FB20B3FA4326E806A009D879ED319AB63EDD0D2D8219AD17CF2BB8311A58EEDB29089352531AB93D85D1B7031352D2B5870C2179BE89A2412076E3C4B14492194DA0E838BC46FD4E8FB5139AD22FBB7B90C6C371B78B203448956D682AA8793D8C78FF61CCEB408DC1E516909D5C0F6AC5DD5CDC19687B58B7794003E1CE2118902633823C4EB320E8BA5AC2C48F0841FEB7FB171B59F73D7808E91E5FB59DD606CC215ECB269A91202A4401D91C256504BF260DDFF4C01524A43C1586C6");
-        byte[] bytes = decryptByPrivateKey(privateKey, en);
-        System.out.println("bytes:" + ByteUtil.bytes2HexString(bytes));
+//        byte[] en = ByteUtil.hexString2Bytes("124DCD2654F46B4296BE416D1B63568AF74A196DD2A2FB993AA6E3D024C6C95AD74F423FA9D48381C09A9807A285F4A37E8C9ABC3974FEF29294FD66563B3DAB8067FB20B3FA4326E806A009D879ED319AB63EDD0D2D8219AD17CF2BB8311A58EEDB29089352531AB93D85D1B7031352D2B5870C2179BE89A2412076E3C4B14492194DA0E838BC46FD4E8FB5139AD22FBB7B90C6C371B78B203448956D682AA8793D8C78FF61CCEB408DC1E516909D5C0F6AC5DD5CDC19687B58B7794003E1CE2118902633823C4EB320E8BA5AC2C48F0841FEB7FB171B59F73D7808E91E5FB59DD606CC215ECB269A91202A4401D91C256504BF260DDFF4C01524A43C1586C6");
+//        byte[] bytes = decryptByPrivateKey(privateKey, en);
+//        System.out.println("bytes:" + ByteUtil.bytes2HexString(bytes));
     }
 
 }
